4d9ff46462c487bfd53781fc620e848032e2f0b8
Nick Kennedy <nick@nick-kennedy.com>
2026-09-15 07:57:27 +0100

    Remove host input wrapper demo

--- Package.swift
+++ Package.swift
@@ -20,7 +20,6 @@ let package = Package(
         .executable(name: "hello-swiftxr", targets: ["HelloSwiftXR"]),
         .executable(name: "minimal-swiftxr-logo", targets: ["MinimalSwiftXRLogo"]),
         .executable(name: "swiftui-panel", targets: ["SwiftUIPanelExample"]),
-        .executable(name: "swiftxr-input-demo", targets: ["SwiftXRInputDemo"]),
     ],
     targets: [
         .systemLibrary(
@@ -52,12 +51,6 @@ let package = Package(
             name: "SwiftUIPanelExample",
             dependencies: ["SwiftXR"],
             path: "Examples/SwiftUIPanel",
-            linkerSettings: openXRExecutableLinkerSettings
-        ),
-        .executableTarget(
-            name: "SwiftXRInputDemo",
-            dependencies: ["SwiftXR"],
-            path: "Examples/HostInputDemo",
             linkerSettings: openXRExecutableLinkerSettings
         ),
     ]
